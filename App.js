7254303ec334cb6e43168574f6ca3e685750103a
X180725 <x180725@MacBook-Pro-de-X180725.local>
2018-12-02 09:54:46 +0100

    Create Bottom TabNavigator

--- App.js
+++ App.js
@@ -1,18 +1,71 @@
-import React from "react";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet
+} from "react-native";
 
-import HomeScreen from './src/screens/HomeScreen';
-import LoginScreen from './src/screens/LoginScreen';
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
 
-const AppNavigator = createStackNavigator({
-  Login: LoginScreen,
-  Home: HomeScreen
-});
-
-const AppContainer = createAppContainer(AppNavigator);
-
-export default class App extends React.Component {
+export class Home extends Component {
   render() {
-    return <AppContainer />;
+    return (
+      <View style={styles.container}>
+        <Text>Hello</Text>
+      </View>
+    );
   }
 }
+export class Settings extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Settings</Text>
+      </View>
+    );
+  }
+}
+const bottomNavigator = createBottomTabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-home" color={tintColor} size={24} />
+      )
+    }
+  },
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-settings" color={tintColor} size={24} />
+      )
+    }
+  }
+
+}, {//router config
+    initialRouteName: 'Settings',
+    order: ['Settings', 'Home'],
+    //navigation for complete tab navigator
+    navigationOptions: {
+      tabBarVisible: true
+    },
+    tabBarOptions: {
+      activeTintColor: 'red',
+      inactiveTintColor: 'grey'
+    }
+  });
+
+
+  export default createAppContainer(bottomNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
